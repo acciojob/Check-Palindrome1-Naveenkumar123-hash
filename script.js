3d8f5575ea16136afa6447f0a6ed26067872b3de
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-06-15 17:25:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+        let start=str.charAt(0);
+	    let end=str.charAt(str.length-1);
+	  while(start>end){
+		  if(start!=end){
+			  return false;
+		  }
+		  else{
+			  start++;
+			  end--;
+		  }
+	  }
+	return true;
 }
 module.exports = palindrome
