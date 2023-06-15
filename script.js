f7e3deead18caa838ca4c4b28965fdc05366c5e5
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-06-15 18:58:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,12 @@
 function palindrome(str){
         let start=0;
 	    let end=str.length-1;
-	  while(start<end){
+	  while(start>=end){
 		  if(str.charAt(start)!==str.charAt(end)){
 			  return false;
 		  }
-		  else {
 		  start++;
 		  end--;
-		  }
 	  }
 	return true;
 }
